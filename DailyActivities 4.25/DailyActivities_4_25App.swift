c649aed7bfee5bd0718089e6d2bdf4dc28008369
Satoshi550 <78008562+Satoshi550@users.noreply.github.com>
2024-04-25 14:05:45 -0500

    Initial Commit

---
+++ DailyActivities 4.25/DailyActivities_4_25App.swift
@@ -0,0 +1,17 @@
+//
+//  DailyActivities_4_25App.swift
+//  DailyActivities 4.25
+//
+//  Created by Satoshi Mitsumori on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyActivities_4_25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
